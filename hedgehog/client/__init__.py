a72cca6fbf20ab3da0079f19cab52db120ebdfaa
Clemens Koza <koza@pria.at>
2016-05-05 22:54:23 +0200

    add more methods to the client; process management is missing

--- hedgehog/client/__init__.py
+++ hedgehog/client/__init__.py
@@ -1,6 +1,6 @@
 import zmq
 from hedgehog.protocol import sockets
-from hedgehog.protocol.messages import analog
+from hedgehog.protocol.messages import ack, analog, digital, motor, servo
 
 
 class HedgehogClient:
@@ -15,6 +15,70 @@ class HedgehogClient:
         response = self.socket.recv()
         assert response.port == port
         return response.value
+
+    def set_analog_state(self, port, pullup):
+        self.socket.send(analog.StateAction(port, pullup))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def get_digital(self, port):
+        self.socket.send(digital.Request(port))
+        response = self.socket.recv()
+        assert response.port == port
+        return response.value
+
+    def set_digital_state(self, port, pullup, output):
+        self.socket.send(digital.StateAction(port, pullup, output))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def set_digital_output(self, port, level):
+        self.socket.send(digital.Action(port, level))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def set_motor(self, port, state, amount=0, reached_state=motor.POWER, relative=None, absolute=None):
+        self.socket.send(motor.Action(port, state, amount, reached_state, relative, absolute))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def move(self, port, amount, state=motor.POWER):
+        self.set_motor(port, state, amount)
+
+    def move_relative_position(self, port, amount, relative, state=motor.POWER):
+        self.set_motor(port, state, amount, relative=relative)
+
+    def move_absolute_position(self, port, amount, absolute, state=motor.POWER):
+        self.set_motor(port, state, amount, absolute=absolute)
+
+    def get_motor(self, port):
+        self.socket.send(motor.Request(port))
+        response = self.socket.recv()
+        assert response.port == port
+        return response.velocity, response.position
+
+    def get_motor_velocity(self, port):
+        velocity, _ = self.get_motor(port)
+        return velocity
+
+    def get_motor_position(self, port):
+        _, position = self.get_motor(port)
+        return position
+
+    def set_motor_position(self, port, position):
+        self.socket.send(motor.SetPositionAction(port, position))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def set_servo(self, port, position):
+        self.socket.send(servo.Action(port, position))
+        response = self.socket.recv()
+        assert response.code == ack.OK
+
+    def set_servo_state(self, port, active):
+        self.socket.send(servo.StateAction(port, active))
+        response = self.socket.recv()
+        assert response.code == ack.OK
 
     def close(self):
         self.socket.close()
--- tests/test.py
+++ tests/test.py
@@ -2,17 +2,126 @@ import unittest
 import zmq
 from hedgehog.server import HedgehogServer, simulator
 from hedgehog.client import HedgehogClient
+from hedgehog.protocol.messages.motor import POWER, BRAKE, VELOCITY
 
 
 class TestClient(unittest.TestCase):
     def test_get_analog(self):
-        context = zmq.Context.instance()
+        context = zmq.Context()
 
-        controller = HedgehogServer('tcp://*:5555', simulator.handler(), context=context)
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
         controller.start()
 
-        client = HedgehogClient('tcp://localhost:5555', context=context)
+        client = HedgehogClient('inproc://controller', context=context)
         self.assertEqual(client.get_analog(0), 0)
+        client.close()
+
+        controller.close()
+
+    def test_set_analog_state(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_analog_state(0, False), None)
+        client.close()
+
+        controller.close()
+
+    def test_get_digital(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.get_digital(0), False)
+        client.close()
+
+        controller.close()
+
+    def test_set_digital_state(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_digital_state(0, False, False), None)
+        client.close()
+
+        controller.close()
+
+    def test_set_digital_output(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_digital_output(0, False), None)
+        client.close()
+
+        controller.close()
+
+    def test_set_motor(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_motor(0, POWER, 100), None)
+        client.close()
+
+        controller.close()
+
+    def test_get_motor(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.get_motor(0), (0, 0))
+        client.close()
+
+        controller.close()
+
+    def test_set_motor_position(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_motor_position(0, 0), None)
+        client.close()
+
+        controller.close()
+
+    def test_set_servo(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_servo(0, 0), None)
+        client.close()
+
+        controller.close()
+
+    def test_set_servo_state(self):
+        context = zmq.Context()
+
+        controller = HedgehogServer('inproc://controller', simulator.handler(), context=context)
+        controller.start()
+
+        client = HedgehogClient('inproc://controller', context=context)
+        self.assertEqual(client.set_servo_state(0, False), None)
         client.close()
 
         controller.close()
