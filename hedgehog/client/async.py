5479601a9a40f5b17b116a7f334236feca3ed3cc
Clemens Koza <koza@pria.at>
2016-05-16 12:36:01 +0200

    make register_cbs a property to better check assertions on usage

--- hedgehog/client/async.py
+++ hedgehog/client/async.py
@@ -1,30 +1,41 @@
-from hedgehog.protocol import messages
 from hedgehog.protocol.messages import motor, process
 
 
 class AsyncRegistry:
     def __init__(self):
-        self.register_cbs = None
+        self._register_cbs = None
         self.motor_cbs = {}
         self.process_cbs = {}
 
+    @property
+    def register_cbs(self):
+        value = self._register_cbs
+        assert value is not None, "register_cbs is not set"
+        self._register_cbs = None
+        return value
+
+    @register_cbs.setter
+    def register_cbs(self, value):
+        assert self._register_cbs is None, "register_cbs is already set"
+        self._register_cbs = value
+
     def handle_register(self, reps):
-        assert len(self.register_cbs) == len(reps)
-        for register, rep in zip(self.register_cbs, reps):
+        register_cbs = self.register_cbs
+        assert len(register_cbs) == len(reps)
+        for register, rep in zip(register_cbs, reps):
             if register is not None:
                 register(rep)
-        self.register_cbs = None
 
     def handle_async(self, backend, msg):
-        if type(msg) is messages.motor.StateUpdate:
+        if type(msg) is motor.StateUpdate:
             reached_cb, = self.process_cbs[msg.pid]
             if reached_cb is not None:
                 backend.spawn(reached_cb, msg.port, msg.state)
-        if type(msg) is messages.process.StreamUpdate:
+        if type(msg) is process.StreamUpdate:
             stream_cb, _ = self.process_cbs[msg.pid]
             if stream_cb is not None:
                 backend.spawn(stream_cb, msg.pid, msg.fileno, msg.chunk)
-        if type(msg) is messages.process.ExitUpdate:
+        if type(msg) is process.ExitUpdate:
             _, exit_cb = self.process_cbs[msg.pid]
             if exit_cb is not None:
                 backend.spawn(exit_cb, msg.pid, msg.exit_code)
