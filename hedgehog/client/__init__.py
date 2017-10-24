6c67c1d31c93095ded66254998943934511e9efe
SillyFreak <sillyfreak.ck@gmail.com>
2017-10-24 12:08:00 +0200

    add TODO about create cleanup

--- hedgehog/client/__init__.py
+++ hedgehog/client/__init__.py
@@ -308,6 +308,8 @@ def connect_create(port='/dev/ttyUSB0', baud=57600):
     create = Create2(port, baud=baud)
     create.start()
     yield create
+    # TODO do proper cleanup instead of waiting for the GC
+    # del create does not seem to work, and create.__del__() raises an exception upon actual GC
 
 
 @contextmanager
@@ -315,3 +317,4 @@ def connect_create2(port='/dev/ttyUSB0', baud=115200):
     create = Create2(port, baud=baud)
     create.start()
     yield create
+    # TODO do proper cleanup instead of waiting for the GC
