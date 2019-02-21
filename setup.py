601c9c47384ebeede70d3e6f6dade5d6f6b8d136
SillyFreak <sillyfreak.ck@gmail.com>
2019-02-21 10:54:01 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='hedgehog-client',
-    version='0.8.0',
+    version='0.9.0rc1',
     description='Client library for communicating with Hedgehog Robot Controllers',
     long_description=long_description,
     url="https://github.com/PRIArobotics/HedgehogClient",
