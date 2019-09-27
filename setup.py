de1358b0a594dd8adcfd6a32351cb80c1e5a7cce
Clemens Koza <koza@pria.at>
2019-09-27 19:38:58 +0200

    update protocol dependency for camera

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['concurrent-utils ~=0.2.0', 'hedgehog-protocol ~=0.9.0',
+    install_requires=['concurrent-utils ~=0.2.0', 'hedgehog-protocol ~=0.10.0rc1',
                       'hedgehog-utils[protobuf,zmq] ~=0.7.0rc1', 'pycreate2'],
 
     # You can install these using the following syntax, for example:
